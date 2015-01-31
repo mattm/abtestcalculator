3204e97f788677dc53aa1f176229da0de8dfe8e3
Matt Mazur <matthew.h.mazur@gmail.com>
2015-01-30 23:20:56 -0500

    Reword analysis for clarity

--- js/abtest-summary.jsx
+++ js/abtest-summary.jsx
@@ -12,32 +12,68 @@ var utils = require( './utils' );
 
 module.exports = React.createClass( {
 	render: function() {
-		var variationAImprovement = Math.round( ( this.props.variations.a.proportion.mean / this.props.variations.b.proportion.mean - 1 ) * 100 ),
+		var variationAConversionRate = Math.round( this.props.variations.a.proportion.mean * 10000 ) / 100,
+			variationBConversionRate = Math.round( this.props.variations.b.proportion.mean * 10000 ) / 100,
+			variationAImprovement = Math.round( ( this.props.variations.a.proportion.mean / this.props.variations.b.proportion.mean - 1 ) * 100 ),
 			variationBImprovement = Math.round( ( this.props.variations.b.proportion.mean / this.props.variations.a.proportion.mean - 1 ) * 100 ),
 			pAGreaterThanB = Math.round( utils.calculateProbabityBIsGratherThanA( this.props.variations.b.proportion, this.props.variations.a.proportion ) * 100 ),
 			pBGreaterThanA = 100 - pAGreaterThanB,
+			winningVariationPossessive,
+			winningVariation,
+			winningConversionRate,
+			losingVariationPossessive,
+			losingVariation,
+			losingConversionRate,
+			percentageImprovement,
+			conversionRatesInWords,
 			changeInWords,
+			isSignificant,
+			oddsOfImprovement,
+			onlyWording,
 			oddsOfImprovementInWords,
 			significanceInWords;
 
-		if ( variationBImprovement < 0 ) {
-			changeInWords = <p><span className="variation-a">Variation A</span> had a <strong>{variationAImprovement}%</strong> higher conversion rate than <span className="variation-b">Variation B</span>.</p>;
-		} else if ( variationBImprovement > 0 ) {
-			changeInWords = <p><span className="variation-b">Variation B</span> had a <strong>{variationBImprovement}%</strong> higher conversion rate than <span className="variation-a">Variation A</span>.</p>;
+		if ( variationBImprovement !== 0 ) {
+			if ( variationBImprovement > 0 ) {
+				winningVariationPossessive = <span className="variation-b">Variation B&#8217;s</span>;
+				winningVariation = <span className="variation-b">Variation B</span>;
+				losingVariationPossessive = <span className="variation-a">Variation A&#8217;s</span>;
+				losingVariation = <span className="variation-a">Variation A</span>;
+				winningConversionRate = variationBConversionRate;
+				losingConversionRate = variationAConversionRate;
+				percentageImprovement = variationBImprovement;
+				oddsOfImprovement = pBGreaterThanA;
+			} else {
+				winningVariationPossessive = <span className="variation-a">Variation A&#8217;s</span>;
+				winningVariation = <span className="variation-a">Variation A</span>;
+				losingVariationPossessive = <span className="variation-b">Variation B&#8217;s</span>;
+				losingVariation = <span className="variation-b">Variation B</span>;
+				winningConversionRate = variationAConversionRate;
+				losingConversionRate = variationBConversionRate;
+				percentageImprovement = variationAImprovement;
+				oddsOfImprovement = pAGreaterThanB;
+			}
+
+			changeInWords = (
+				<p>
+					{ winningVariationPossessive } observed conversion rate ({ winningConversionRate }%) was { percentageImprovement }% higher than { losingVariationPossessive } conversion rate ({ losingConversionRate }%).
+				</p>
+			);
 		} else {
 			changeInWords = <p><span className="variation-b">Variation B</span> had the same conversion rate as <span className="variation-a">Variation A</span>.</p>;
+			winningVariation = <span className="variation-b">Variation B</span>;
+			losingVariation = <span className="variation-a">Variation A</span>;
+			oddsOfImprovement = pBGreaterThanA;
 		}
 
-		if ( variationBImprovement < 0 ) {
-			oddsOfImprovementInWords = <p>There is a <strong>{pAGreaterThanB}%</strong> chance that <span className="variation-a">Variation A</span> has a higher conversion rate.</p>;
-		} else {
-			oddsOfImprovementInWords = <p>There is a <strong>{pBGreaterThanA}%</strong> chance that <span className="variation-b">Variation B</span> has a higher conversion rate.</p>;
-		}
+		isSignificant = pBGreaterThanA >= 80 || pAGreaterThanB >= 80;
+		onlyWording = isSignificant ? '' : 'only ';
+		oddsOfImprovementInWords = <p>There is {onlyWording}a { oddsOfImprovement }% chance that { winningVariation } really does has a higher conversion rate than { losingVariation }.</p>;
 
-		if ( pBGreaterThanA >= 80 || pAGreaterThanB >= 80 ) {
-			significanceInWords = <p>Your A/B test is statistically significant!</p>;
+		if ( isSignificant ) {
+			significanceInWords = <p>This means your A/B test <i>is</i> statistically significant!</p>;
 		} else {
-			significanceInWords = <p>Your A/B test is NOT statistically significant.</p>;
+			significanceInWords = <p>This means your A/B test <i>is NOT</i> statistically significant.</p>;
 		}
 
 		return (
