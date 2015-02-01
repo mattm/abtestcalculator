87e0708d2437f15cea42f17862ce902f737f7a7c
Matt Mazur <matthew.h.mazur@gmail.com>
2015-01-31 21:26:35 -0500

    Add analytics event for copying results url

--- js/copy-url-button.jsx
+++ js/copy-url-button.jsx
@@ -4,6 +4,11 @@
 var React = require( 'react' ),
 	ReactZeroClipboard = require( 'react-zeroclipboard' );
 
+/**
+ * Internal dependencies
+ */
+var analytics = require( './analytics' );
+
 module.exports = React.createClass( {
 	getResultsURL: function() {
 		return 'http://www.abtestcalculator.com' +
@@ -14,6 +19,7 @@ module.exports = React.createClass( {
 	},
 
 	urlCopied: function() {
+		analytics.recordEvent( 'copy results url' );
 		alert( 'The URL for these A/B test results was successfully copied to your clipboard.' );
 	},
 
