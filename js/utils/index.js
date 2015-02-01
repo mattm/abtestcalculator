2d585ee16300ec1e73e3b87867852e8790e51411
Matt Mazur <matthew.h.mazur@gmail.com>
2015-01-31 21:12:01 -0500

    Remove superfluous var statements from error function

--- js/utils/index.js
+++ js/utils/index.js
@@ -28,18 +28,18 @@ module.exports = {
 
 	// Adapted from http://picomath.org/javascript/erf.js.html
 	erf: function( x ) {
-		var a1 = 0.254829592;
-		var a2 = -0.284496736;
-		var a3 = 1.421413741;
-		var a4 = -1.453152027;
-		var a5 = 1.061405429;
-		var p = 0.3275911;
+		var a1 = 0.254829592,
+			a2 = -0.284496736,
+			a3 = 1.421413741,
+			a4 = -1.453152027,
+			a5 = 1.061405429,
+			p = 0.3275911,
+			sign = x < 0 ? -1 : 1,
+			t, y;
 
-		var sign = x < 0 ? -1 : 1;
 		x = Math.abs( x );
-
-		var t = 1 / (1 + p * x);
-		var y = 1 - ( ( ( ( ( a5 * t + a4 ) * t ) + a3 ) * t + a2 ) * t + a1 ) * t * Math.exp( -x * x );
+		t = 1 / (1 + p * x);
+		y = 1 - ( ( ( ( ( a5 * t + a4 ) * t ) + a3 ) * t + a2 ) * t + a1 ) * t * Math.exp( -x * x );
 
 		return sign * y;
 	}
