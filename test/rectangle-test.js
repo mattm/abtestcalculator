a33da364318243be5e8838722d678fee21fea3ce
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-14 12:00:46 -0500

    Move variable declaration

--- test/rectangle-test.js
+++ test/rectangle-test.js
@@ -10,10 +10,10 @@ var assert = require( 'assert' );
  */
 var Rectangle = require( '../js/graphs/rectangle' );
 
-var testRectangle = new Rectangle( 100, 200, 50, 75 );
-
 describe( 'rectangle', function() {
 	it( 'should set all the props', function() {
+		var testRectangle = new Rectangle( 100, 200, 50, 75 );
+
 		assert.equal( testRectangle.x, 100 );
 		assert.equal( testRectangle.y, 200 );
 		assert.equal( testRectangle.width, 50 );
