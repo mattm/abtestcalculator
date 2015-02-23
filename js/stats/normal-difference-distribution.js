0628fb38988ceaf080df5ea9b04ce9666f31550b
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-22 19:52:16 -0500

    Convert Normal Difference Distribution class to ES6

--- js/stats/normal-difference-distribution.js
+++ js/stats/normal-difference-distribution.js
@@ -6,13 +6,12 @@
 let NormalDistribution = require( './normal-distribution' );
 
 // See: http://mathworld.wolfram.com/NormalDifferenceDistribution.html
-function NormalDifferenceDistribution( proportionA, proportionB ) {
-	let mean = proportionB.mean - proportionA.mean,
-		sd = Math.sqrt( proportionA.variance + proportionB.variance );
-	NormalDistribution.call( this, mean, sd );
+class NormalDifferenceDistribution extends NormalDistribution {
+	constructor( proportionA, proportionB ) {
+		let mean = proportionB.mean - proportionA.mean,
+			sd = Math.sqrt( proportionA.variance + proportionB.variance );
+		super( mean, sd );
+	}
 }
-
-NormalDifferenceDistribution.prototype = Object.create( NormalDistribution.prototype );
-NormalDifferenceDistribution.prototype.constructor = NormalDifferenceDistribution;
 
 module.exports = NormalDifferenceDistribution;
