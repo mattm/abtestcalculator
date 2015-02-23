1c8a38094003e3398a468877efafdb7367a36083
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-22 19:32:41 -0500

    Convert Variation class to ES6

--- js/stats/variation.js
+++ js/stats/variation.js
@@ -5,20 +5,22 @@
  */
 let SampleProportion = require( './sample-proportion' );
 
-function Variation( name, color, participants, conversions ) {
-	this.name = name;
-	this.color = color;
-	this.participants = participants;
-	this.conversions = conversions;
-	this.proportion = new SampleProportion( participants, conversions );
+class Variation {
+	constructor( name, color, participants, conversions ) {
+		this.name = name;
+		this.color = color;
+		this.participants = participants;
+		this.conversions = conversions;
+		this.proportion = new SampleProportion( participants, conversions );
+	}
+
+	// See: http://blog.42floors.com/math-split-testing-part-2-chance-better/
+	isGaussian() {
+		let np = this.participants * this.proportion.mean,
+			nq = this.participants * ( 1 - this.proportion.mean );
+
+		return this.participants >= 30 && np >= 5 && nq >= 5;
+	};
 }
-
-// See: http://blog.42floors.com/math-split-testing-part-2-chance-better/
-Variation.prototype.isGaussian = function() {
-	let np = this.participants * this.proportion.mean,
-		nq = this.participants * ( 1 - this.proportion.mean );
-
-	return this.participants >= 30 && np >= 5 && nq >= 5;
-};
 
 module.exports = Variation;
