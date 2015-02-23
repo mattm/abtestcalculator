dc5d27010673a53c6cf48b5413cf2d348829e767
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-22 20:16:40 -0500

    Convert Sample Proportions Graph Renderer to ES6

--- js/graphs/sample-proportions-graph-renderer.js
+++ js/graphs/sample-proportions-graph-renderer.js
@@ -12,122 +12,121 @@ let colorUtils = require( '../utils/color-utils' ),
 	GraphRenderer = require( './graph-renderer' ),
 	NumberRange = require( '../stats/number-range' );
 
-function SampleProportionsGraphRenderer( context ) {
-	GraphRenderer.call( this, context );
+class SampleProportionsGraphRenderer extends GraphRenderer {
+	constructor( context ) {
+		super( context );
+	}
+
+	render() {
+		this.calculateAxisRanges();
+		this.renderSampleProportions();
+		super.render();
+	}
+
+	calculateXAxisRange() {
+		let exactRange = this.calculateXAxisRangeExact();
+
+		this.xAxisRange = new NumberRange(
+			Math.floor( exactRange.min / this.getXAxisInterval() ) * this.getXAxisInterval(),
+			Math.ceil( exactRange.max / this.getXAxisInterval() ) * this.getXAxisInterval()
+		);
+	}
+
+	calculateXAxisRangeExact() {
+		let xAxisRange, min, max;
+
+		this.variations.forEach( function( variation ) {
+			xAxisRange = variation.proportion.xRange;
+			if ( min === undefined || xAxisRange.min < min ) {
+				min = xAxisRange.min;
+			}
+			if ( max === undefined || xAxisRange.max > max ) {
+				max = xAxisRange.max;
+			}
+		} );
+		return new NumberRange( min, max );
+	}
+
+	getXAxisInterval() {
+		let width = this.calculateXAxisRangeExact().getWidth();
+
+		if ( width < 0.05 ) {
+			return 0.01;
+		} else if ( width < 0.3 ) {
+			return 0.05;
+		} else {
+			return 0.10;
+		}
+	}
+
+	calculateYAxisRange() {
+		let yAxisRange, max;
+
+		this.variations.forEach( function( variation ) {
+			yAxisRange = variation.proportion.getYAxisRange();
+			if ( max === undefined || yAxisRange.max > max ) {
+				max = yAxisRange.max;
+			}
+		} );
+		this.yAxisRange = new NumberRange( 0, max );
+	}
+
+	renderSampleProportions() {
+		this.variations.forEach( function ( variation ) {
+			this.renderSamplingDistribution( variation );
+		}, this );
+	}
+
+	renderSamplingDistribution( variation ) {
+		this.renderSampleDistributionFill( variation );
+		this.renderSampleDistributionOutline( variation );
+		this.renderCenter( variation );
+	}
+
+	renderSampleDistributionOutline( variation ) {
+		this.ctx.lineWidth = this.OUTLINE_LINE_WIDTH;
+		this.ctx.strokeStyle = colorUtils.hexToTransparentRGB( variation.color, this.OUTLINE_OPACITY );
+		this.ctx.beginPath();
+		this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.xRange.min), this.rect.bottom );
+		variation.proportion.xValues.forEach( function( xValue, i ) {
+			this.ctx.lineTo( this.distributionXToCanvasX( xValue ), this.distributionYToCanvasY( variation.proportion.yValues[ i ] ) );
+		}, this );
+		this.ctx.stroke();
+	}
+
+	renderSampleDistributionFill( variation ) {
+		this.ctx.beginPath();
+		this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.xRange.min), this.rect.bottom );
+		variation.proportion.xValues.forEach( function( xValue, i ) {
+			this.ctx.lineTo( this.distributionXToCanvasX( xValue ), this.distributionYToCanvasY( variation.proportion.yValues[ i ] ) );
+		}, this );
+		this.ctx.closePath();
+		this.ctx.fillStyle = colorUtils.hexToTransparentRGB( variation.color, this.FILL_OPACITY );
+		this.ctx.fill();
+	}
+
+	renderCenter( variation ) {
+		this.ctx.beginPath();
+		this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.mean ), this.rect.bottom );
+		this.ctx.lineTo( this.distributionXToCanvasX( variation.proportion.mean ), this.distributionYToCanvasY( variation.proportion.getPeakDensity() ) );
+		this.ctx.closePath();
+		this.ctx.lineWidth = 1;
+		this.ctx.strokeStyle = colorUtils.hexToTransparentRGB( variation.color, 0.5 );
+		this.ctx.stroke();
+	}
+
+	renderAxisValues() {
+		let numTicks, canvasY;
+
+		numTicks = this.xAxisRange.getWidth() / this.getXAxisInterval() + 1;
+		canvasY = this.rect.bottom + this.X_AXIS_TICK_FONT_SIZE + this.X_AXIS_TICK_MARGIN_TOP;
+		for ( let i = 0, l = numTicks; i < l; i++ ) {
+			let proportion = this.xAxisRange.min + i * this.getXAxisInterval(),
+				percentage = numeral( proportion ).format( '0%' ),
+				canvasX = this.rect.x + ( i / ( numTicks - 1 ) ) * this.rect.width;
+			this.renderAxisTextWithTick( percentage, canvasX, canvasY );
+		}
+	}
 }
-
-SampleProportionsGraphRenderer.prototype = Object.create( GraphRenderer.prototype );
-SampleProportionsGraphRenderer.prototype.constructor = SampleProportionsGraphRenderer;
-
-SampleProportionsGraphRenderer.prototype.render = function() {
-	this.calculateAxisRanges();
-	this.renderSampleProportions();
-	GraphRenderer.prototype.render.call( this );
-};
-
-SampleProportionsGraphRenderer.prototype.calculateXAxisRange = function() {
-	let exactRange = this.calculateXAxisRangeExact();
-
-	this.xAxisRange = new NumberRange(
-		Math.floor( exactRange.min / this.getXAxisInterval() ) * this.getXAxisInterval(),
-		Math.ceil( exactRange.max / this.getXAxisInterval() ) * this.getXAxisInterval()
-	);
-};
-
-SampleProportionsGraphRenderer.prototype.calculateXAxisRangeExact = function() {
-	let xAxisRange, min, max;
-
-	this.variations.forEach( function( variation ) {
-		xAxisRange = variation.proportion.xRange;
-		if ( min === undefined || xAxisRange.min < min ) {
-			min = xAxisRange.min;
-		}
-		if ( max === undefined || xAxisRange.max > max ) {
-			max = xAxisRange.max;
-		}
-	} );
-	return new NumberRange( min, max );
-};
-
-SampleProportionsGraphRenderer.prototype.getXAxisInterval = function() {
-	let width = this.calculateXAxisRangeExact().getWidth();
-
-	if ( width < 0.05 ) {
-		return 0.01;
-	} else if ( width < 0.3 ) {
-		return 0.05;
-	} else {
-		return 0.10;
-	}
-};
-
-SampleProportionsGraphRenderer.prototype.calculateYAxisRange = function() {
-	let yAxisRange, max;
-
-	this.variations.forEach( function( variation ) {
-		yAxisRange = variation.proportion.getYAxisRange();
-		if ( max === undefined || yAxisRange.max > max ) {
-			max = yAxisRange.max;
-		}
-	} );
-	this.yAxisRange = new NumberRange( 0, max );
-};
-
-SampleProportionsGraphRenderer.prototype.renderSampleProportions = function() {
-	this.variations.forEach( function ( variation ) {
-		this.renderSamplingDistribution( variation );
-	}, this );
-};
-
-SampleProportionsGraphRenderer.prototype.renderSamplingDistribution = function( variation ) {
-	this.renderSampleDistributionFill( variation );
-	this.renderSampleDistributionOutline( variation );
-	this.renderCenter( variation );
-};
-
-SampleProportionsGraphRenderer.prototype.renderSampleDistributionOutline = function( variation ) {
-	this.ctx.lineWidth = this.OUTLINE_LINE_WIDTH;
-	this.ctx.strokeStyle = colorUtils.hexToTransparentRGB( variation.color, this.OUTLINE_OPACITY );
-	this.ctx.beginPath();
-	this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.xRange.min), this.rect.bottom );
-	variation.proportion.xValues.forEach( function( xValue, i ) {
-		this.ctx.lineTo( this.distributionXToCanvasX( xValue ), this.distributionYToCanvasY( variation.proportion.yValues[ i ] ) );
-	}, this );
-	this.ctx.stroke();
-};
-
-SampleProportionsGraphRenderer.prototype.renderSampleDistributionFill = function( variation ) {
-	this.ctx.beginPath();
-	this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.xRange.min), this.rect.bottom );
-	variation.proportion.xValues.forEach( function( xValue, i ) {
-		this.ctx.lineTo( this.distributionXToCanvasX( xValue ), this.distributionYToCanvasY( variation.proportion.yValues[ i ] ) );
-	}, this );
-	this.ctx.closePath();
-	this.ctx.fillStyle = colorUtils.hexToTransparentRGB( variation.color, this.FILL_OPACITY );
-	this.ctx.fill();
-};
-
-SampleProportionsGraphRenderer.prototype.renderCenter = function( variation ) {
-	this.ctx.beginPath();
-	this.ctx.moveTo( this.distributionXToCanvasX( variation.proportion.mean ), this.rect.bottom );
-	this.ctx.lineTo( this.distributionXToCanvasX( variation.proportion.mean ), this.distributionYToCanvasY( variation.proportion.getPeakDensity() ) );
-	this.ctx.closePath();
-	this.ctx.lineWidth = 1;
-	this.ctx.strokeStyle = colorUtils.hexToTransparentRGB( variation.color, 0.5 );
-	this.ctx.stroke();
-};
-
-SampleProportionsGraphRenderer.prototype.renderAxisValues = function() {
-	let numTicks, canvasY;
-
-	numTicks = this.xAxisRange.getWidth() / this.getXAxisInterval() + 1;
-	canvasY = this.rect.bottom + this.X_AXIS_TICK_FONT_SIZE + this.X_AXIS_TICK_MARGIN_TOP;
-	for ( let i = 0, l = numTicks; i < l; i++ ) {
-		let proportion = this.xAxisRange.min + i * this.getXAxisInterval(),
-			percentage = numeral( proportion ).format( '0%' ),
-			canvasX = this.rect.x + ( i / ( numTicks - 1 ) ) * this.rect.width;
-		this.renderAxisTextWithTick( percentage, canvasX, canvasY );
-	}
-};
 
 module.exports = SampleProportionsGraphRenderer;
