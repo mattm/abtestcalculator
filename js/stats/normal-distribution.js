0d5d01b809f356e3c143005f421c906f0e044b13
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-22 19:49:41 -0500

    Convert Normal Distribution class to ES6

--- js/stats/normal-distribution.js
+++ js/stats/normal-distribution.js
@@ -5,65 +5,65 @@
  */
 let NumberRange = require( './number-range' );
 
-function NormalDistribution( mean, sd ) {
-	this.mean = mean;
-	this.sd = sd;
-}
+const SD_TO_GRAPH = 4;
+const POINTS_PER_CURVE = 1000;
 
-NormalDistribution.prototype = {
-	SD_TO_GRAPH: 4,
-	POINTS_PER_CURVE: 1000,
+class NormalDistribution {
+	constructor( mean, sd ) {
+		this.mean = mean;
+		this.sd = sd;
+	}
 
-	getX: function( z ) {
+	getX( z ) {
 		return this.mean + z * this.sd;
-	},
+	}
 
-	getRange: function( z ) {
+	getRange( z ) {
 		return new NumberRange( this.getX( -z ), this.getX( z ) );
-	},
+	}
 
-	getYAxisRange: function() {
+	getYAxisRange() {
 		return new NumberRange( 0, this.getDensity( this.mean ) );
-	},
+	}
 
 	// See: See: http://en.wikipedia.org/wiki/Normal_distribution
-	getDensity: function( x ) {
+	getDensity( x ) {
 		let a = 1 / ( this.sd * Math.sqrt( 2 * Math.PI ) ),
 			b = Math.exp( -( Math.pow( x - this.mean, 2) ) / ( 2 * Math.pow( this.sd, 2 ) ) );
 
 		return a * b;
-	},
+	}
 
-	getPeakDensity: function() {
+	getPeakDensity() {
 		return this.getDensity( this.mean );
-	},
+	}
 
-	getCurveXValues: function() {
+	getCurveXValues() {
 		let xRange = this.xRange,
-			range = new NumberRange( xRange.min, xRange.max, this.POINTS_PER_CURVE );
+			range = new NumberRange( xRange.min, xRange.max, POINTS_PER_CURVE );
 
 		return range.getValues();
-	},
+	}
 
-	getCurveYValues: function() {
+	getCurveYValues() {
 		return this.xValues.map( function( xValue ) {
 			return this.getDensity( xValue );
 		}, this );
-	},
+	}
 
-	getXBetween: function( minExclusive, maxInclusive ) {
+	getXBetween( minExclusive, maxInclusive ) {
 		return this.xValues.filter( function( xValue ) {
 			return xValue > minExclusive && xValue <= maxInclusive;
 		} );
-	},
+	}
 
-	getYForXBetween: function( minExclusive, maxInclusive ) {
+	getYForXBetween( minExclusive, maxInclusive ) {
 		let xValues = this.getXBetween( minExclusive, maxInclusive );
 		return xValues.map( function( xValue ) {
 			return this.getDensity( xValue );
 		}, this );
-	},
-};
+	}
+}
 
 Object.defineProperties( NormalDistribution.prototype, {
 	'variance': {
@@ -74,7 +74,7 @@ Object.defineProperties( NormalDistribution.prototype, {
 	'xRange': {
 		get: function() {
 			if ( this._xRange === undefined ) {
-				this._xRange = this.getRange( this.SD_TO_GRAPH );
+				this._xRange = this.getRange( SD_TO_GRAPH );
 			}
 			return this._xRange;
 		}
@@ -82,7 +82,7 @@ Object.defineProperties( NormalDistribution.prototype, {
 	'xValues': {
 		get: function() {
 			if ( this._xValues === undefined ) {
-				let range = new NumberRange( this.xRange.min, this.xRange.max, this.POINTS_PER_CURVE );
+				let range = new NumberRange( this.xRange.min, this.xRange.max, POINTS_PER_CURVE );
 				this._xValues = range.getValues();
 			}
 			return this._xValues;
