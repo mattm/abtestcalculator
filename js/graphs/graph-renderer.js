9862c82bfcc2b47026904599905e6c93e01428b0
Matt Mazur <matthew.h.mazur@gmail.com>
2015-02-22 20:16:11 -0500

    Convert Graph Renderer class to ES6

--- js/graphs/graph-renderer.js
+++ js/graphs/graph-renderer.js
@@ -1,88 +1,90 @@
 'use strict';
 
-function GraphRenderer( context ) {
-	this.ctx = context;
-}
+class GraphRenderer {
+	constructor( context ) {
+		this.ctx = context;
 
-GraphRenderer.prototype = {
-	OUTLINE_LINE_WIDTH: 2,
-	OUTLINE_OPACITY: 1,
-	FILL_OPACITY: 0.7,
+		// Probably a better way to do this
+		// Or not: http://stackoverflow.com/questions/22528967/es6-class-variable-alternatives
+		this.OUTLINE_LINE_WIDTH = 2;
+		this.OUTLINE_OPACITY = 1;
+		this.FILL_OPACITY = 0.7;
 
-	X_AXIS_INTERVAL: 0.05,
-	X_AXIS_TICK_FONT_SIZE: 11,
-	X_AXIS_TICK_TICKER_HEIGHT: 6,
-	X_AXIS_TICK_MARGIN_TOP: 8,
-	X_AXIS_TICK_COLOR: '#bbb',
-	X_AXIS_TEXT_COLOR: '#ccc',
+		this.X_AXIS_INTERVAL = 0.05;
+		this.X_AXIS_TICK_FONT_SIZE = 11;
+		this.X_AXIS_TICK_TICKER_HEIGHT = 6;
+		this.X_AXIS_TICK_MARGIN_TOP = 8;
+		this.X_AXIS_TICK_COLOR = '#bbb';
+		this.X_AXIS_TEXT_COLOR = '#ccc';
+	}
 
-	setRect: function( rect ) {
+	setRect( rect ) {
 		this.rect = rect;
-	},
+	}
 
-	renderBackground: function() {
+	renderBackground() {
 		this.ctx.fillStyle = 'rgba(0,0,0,0)';
 		this.ctx.clearRect( this.rect.x, this.rect.y, this.rect.width, this.rect.height );
-	},
+	}
 
-	setVariations: function( variations ) {
+	setVariations( variations ) {
 		this.variations = [ variations.a, variations.b ];
-	},
+	}
 
-	getControl: function() {
+	getControl() {
 		return this.variations[ 0 ];
-	},
+	}
 
-	getExperiment: function() {
+	getExperiment() {
 		return this.variations[ 1 ];
-	},
+	}
 
-	distributionXToCanvasX: function( distributionX ) {
+	distributionXToCanvasX( distributionX ) {
 		return this.rect.x + this.rect.width * (distributionX - this.xAxisRange.min) / this.xAxisRange.getWidth();
-	},
+	}
 
-	distributionYToCanvasY: function( distributionY ) {
+	distributionYToCanvasY( distributionY ) {
 		return this.rect.bottom - this.rect.height * distributionY / this.yAxisRange.max;
-	},
+	}
 
-	render: function() {
+	render() {
 		this.renderAxis();
-	},
+	}
 
-	calculateAxisRanges: function() {
+	calculateAxisRanges() {
 		this.calculateXAxisRange();
 		this.calculateYAxisRange();
-	},
+	}
 
-	renderAxis: function() {
+	renderAxis() {
 		this.renderAxisLine();
 		this.renderAxisValues();
-	},
+	}
 
-	renderAxisLine: function() {
+	renderAxisLine() {
 		this.ctx.beginPath();
 		this.ctx.moveTo( this.rect.x, this.rect.bottom + 0.5 );
 		this.ctx.lineTo( this.rect.right, this.rect.bottom + 0.5 );
 		this.ctx.lineWidth = 1;
 		this.ctx.strokeStyle = this.X_AXIS_TICK_COLOR;
 		this.ctx.stroke();
-	},
+	}
 
-	renderTick: function( canvasX ) {
+	renderTick( canvasX ) {
 		this.ctx.lineWidth = 1;
 		this.ctx.strokeStyle = this.X_AXIS_TICK_COLOR;
 		this.ctx.beginPath();
 		this.ctx.moveTo( canvasX, this.rect.bottom );
 		this.ctx.lineTo( canvasX, this.rect.bottom + this.X_AXIS_TICK_TICKER_HEIGHT );
 		this.ctx.stroke();
-	},
+	}
 
-	renderAxisTextWithTick: function( value, canvasX, canvasY ) {
+	renderAxisTextWithTick( value, canvasX, canvasY ) {
 		this.renderAxisText( value, canvasX, canvasY );
 		this.renderTick( canvasX );
-	},
+	}
 
-	renderAxisText: function( value, canvasX, canvasY ) {
+	renderAxisText( value, canvasX, canvasY ) {
 		this.ctx.font = this.X_AXIS_TICK_FONT_SIZE + 'px Helvetica';
 		this.ctx.textAlign = 'center';
 		this.ctx.fillStyle = this.X_AXIS_TEXT_COLOR;
